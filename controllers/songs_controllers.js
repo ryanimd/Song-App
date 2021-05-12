d60fd6b0d9d5b509c7a6efe1501f7cce4eddc22c
Kausar Akther <kausar.akther@gmail.com>
2021-05-12 10:18:34 -0400

    add controllers.js

--- controllers/songs_controllers.js
+++ controllers/songs_controllers.js
@@ -3,11 +3,42 @@ const song = express.Router()
 const Entry = require('../models/song.js')
 
 
+song.get('/', (req, res) => {
+    Entry.find({}, (err, foundEntry) => {
+        res.json(foundEntry)
+    })
+})
+
+song.post('/', (req, res) => {
+    Entry.create(req.body, (err, createEntry) => {
+        Entry.find({}, (err, foundEntry) => {
+            res.json(foundEntry)
+        })
+    })
+})
 
 
+song.put('/:id', (req, res) => {
+    Entry.findByIdAndUpdate(req.params.id, req.body, {new: true},
+        (err, updateEntry) => {
+            if(err){
+                res.send(err)
+            }else{
+                Entry.findByIdAndUpdate({}, (err, foundEntry) => {
+                    res.json(foundEntry)
+                })
+            }
+        })
+})
 
 
-
+song.debug('/:id', (req, res) => {
+    Entry.findByIdAndRemove(req.params.id, (err, deletedEntry) => {
+        Entry.find({}, (err, foundEntry) => {
+            res.json(foundEntry)
+        })
+    })
+})
 
 
 module.exports = song;
