1099758109083483fa3b65139f56429b2b23680f
Kausar Akther <kausar.akther@gmail.com>
2021-05-11 23:09:45 -0400

    update server.js and add files

--- server.js
+++ server.js
@@ -13,10 +13,31 @@ const MONGODB_URI = process.env.MONGODB_URI
 app.use(express.static('public'))
 app.use(express.json())
 const songController = require('./controllers/songs_controller.js')
-app.use('/songs', songController)
+app.use('/song', songController)
 
 
 //LISTENER
 app.listen(PORT, () => {
     console.log('listening on Port', PORT)
 })
+
+
+//MONGODB CONNECTION
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+
+  mongoose.connection.on('error', err =>
+  console.log(
+      err.message,
+      'is Mongod not running?/Problem with Atlas Connection?'
+      )
+
+  )
+
+  mongoose.connection.on('connected', () =>
+    console.log('mongo connected: ', MONGODB_URI)
+  )
+  mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
